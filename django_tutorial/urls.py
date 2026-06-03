0c3a02553103df416afd745f96e1048953aefc22
Hélio Junior <110945316+Heliojrdevs@users.noreply.github.com>
2026-06-03 00:11:42 -0300

    feat: integracao da API REST com JWT e resolucao da foto de perfil

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('portfolio/', include('portfolio.urls')),
+
+    # API do Perfil (Rotas novas do Core)
+    path('', include('core.urls')),
+
+    # Autenticacao JWT (Cria os endpoints para login e renovacao de token)
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
