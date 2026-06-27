8d8112c5eafc8a061143f569f09da5eba5a3d9c3
Hélio Junior <dasilvajuniorjosehelio@gmail.com>
2026-06-26 21:32:38 -0300

    feat: 2VA parte dois

--- portfolio/models.py
+++ portfolio/models.py
@@ -18,3 +18,16 @@ class Projeto(models.Model):
 
     def __str__(self):
         return self.nome
+    
+class Pessoal(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    curso = models.CharField(max_length=150)
+    periodo = models.CharField(max_length=50)
+    email = models.EmailField()
+    git = models.URLField()
+    linked = models.URLField()
+    imagem_url = models.URLField()
+
+    def __str__(self):
+        return self.nome
